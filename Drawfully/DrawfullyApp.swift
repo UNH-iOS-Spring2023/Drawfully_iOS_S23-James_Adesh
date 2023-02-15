7c326c8e87c575d1ffdf9a411fc51432250c13b8
adeshagarwal28 <59112633+adeshagarwal28@users.noreply.github.com>
2023-02-14 19:08:11 -0500

    Initial Commit

---
+++ Drawfully/DrawfullyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawfullyApp.swift
+//  Drawfully
+//
+//  Created by Adesh Agarwal on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawfullyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
