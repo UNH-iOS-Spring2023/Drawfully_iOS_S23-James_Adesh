ef24bc63e65ffc6ba3903916c8553f5b02bdce2f
hoodz <jmok780@gmail.com>
2023-03-13 03:03:14 -0400

    allow notifications to be disabled in settings

--- Drawfully/Settings/Settings.swift
+++ Drawfully/Settings/Settings.swift
@@ -8,23 +8,44 @@
 import SwiftUI
 
 struct Settings: View {
+    @State private var notifs = UserDefaults.standard.bool(forKey: "notification")
+    // see if notifications are on or not
+    
     var body: some View {
         
         //Add title bar to Settings Tab
-            VStack{
-                
+        let header = VStack{
                 HStack{
-                    
                     Spacer()
                     Text("Settings").font(.title).fontWeight(.bold).padding(.trailing, 0.0).multilineTextAlignment(.center)
                     Spacer()
-                    Image(systemName: "magnifyingglass")
                 }.padding()
                 
-                Spacer()
-                
-            }
-            }
+        }
+        
+        let menu = VStack(alignment: .leading){
+            // toggle if you want to receive daily notification: see Notifications.swift
+            HStack{
+                Toggle(isOn: $notifs){
+                    Text("Notifications")
+                    Text("Toggle daily reminder").font(.system(size: 20))
+                }.onChange(of: notifs){ _notifs in // I don't really understand why it needs to be _notifs
+                    if _notifs { doNotification() }
+                    else { removeNotification() }
+                }
+            }.padding()
+            Text("Change Username").padding() // TODO
+            Text("Change Email").padding() // TODO
+        }.padding().font(.system(size: 30))
+        
+        VStack{
+            header
+            Divider()
+            Spacer()
+            menu
+            Spacer()
+        }
+        }
     
 }
 
@@ -33,3 +54,4 @@ struct Settings_Previews: PreviewProvider {
         Settings()
     }
 }
+
