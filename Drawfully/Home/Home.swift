d0660a62ff8501f775267e5609a03a526316b56d
adeshagarwal28 <59112633+adeshagarwal28@users.noreply.github.com>
2023-03-18 12:17:40 -0400

    Importing images from Cloud Storage and Displaying
    Displaying in rows of 3

--- Drawfully/Home/Home.swift
+++ Drawfully/Home/Home.swift
@@ -14,6 +14,14 @@ import Firebase
 //Creating data model for fetching and storing user data
 struct AppUser{
     let uid,streak,email,firstName,lastName,username: String
+    //let drawings: Array<Any>
+    @State var drawings = [DocumentReference]()
+}
+
+//Creating data model for fetching and storing user data
+struct DrawingPost{
+    let uid,caption,likes,title: String
+    let isPublic: Bool
 }
 
 
@@ -23,12 +31,16 @@ class HomeViewModel: ObservableObject{
     
     @Published var errorMessage:String=""
     @Published var streakCount:String="T"
-    @Published var CurrentUser:AppUser=AppUser(uid: "", streak: "", email: "", firstName: "", lastName: "", username: "")
+    @Published var CurrentUser:AppUser=AppUser(uid: "", streak: "", email: "", firstName: "", lastName: "", username: "", drawings: [])
+    @Published var drawing:DrawingPost=DrawingPost(uid:"", caption: "", likes: "", title: "", isPublic: false)
+    @Published var posts=[DrawingPost]()
     
+    @Published var drawingImages=[UIImage]()
     
     init()
     {
         fetchCurrentUser()
+        //getDrawingImage(x:"")
     }
     
     private func fetchCurrentUser(){
@@ -42,20 +54,62 @@ class HomeViewModel: ObservableObject{
             guard let data=snapshot?.data() else {
                 return}
             
-            let appUser = AppUser(uid: uid, streak: data["streak"] as? String ?? "", email: data["email"] as? String ?? "", firstName: data["FirstName"] as? String ?? "", lastName: data["LastName"] as? String ?? "", username: data["username"] as? String ?? "")
+            let appUser = AppUser(uid: uid, streak: data["streak"] as? String ?? "", email: data["email"] as? String ?? "", firstName: data["FirstName"] as? String ?? "", lastName: data["LastName"] as? String ?? "", username: data["username"] as? String ?? "", drawings: data["drawings"] as? [DocumentReference] ?? [] )
             self.CurrentUser=appUser
+            print(appUser.drawings.count)
+            
+            for doc in appUser.drawings{
+                print(doc)
+                print(doc.path)
+                doc.getDocument{ document, error in
+                    if let error = error {
+                        print("Error getting document: \(error)")
+                    } else if let document = document {
+                        let art = DrawingPost(uid: document.documentID , caption: document["Caption"] as? String ?? "", likes: document["Likes"]as? String ?? "", title: document["Title"] as? String ?? "", isPublic: document["isPublic"] as? Bool ?? false)
+                        self.posts.append(art)
+                        print(art)
+                        self.getDrawingImage(x: art.uid)
+                        //drawingImages.append(getDrawingImage(x: art.uid) ?? Image("sample_drawing"))
+                    }
+                }
+            }
+            
+            print(self.posts.count)
+            
             
         }
     }
+    
+    
+    // Citation : https://firebase.google.com/docs/storage/ios/download-files
+    func getDrawingImage(x:String)
+    {
+        // Create a reference with an initial file path and name
+        let pathReference = FirebaseManager.shared.storage.reference(withPath: "drawings/\(self.CurrentUser.uid)/\(x)")
+        pathReference.getData(maxSize: 2 * 1024 * 1024){ data, error in
+            if let error = error {
+                // Uh-oh, an error occurred!
+                print("Error getting drawing image : \(error)")
+                return()
+            }
+            else{
+                let image = UIImage(data: data!)
+                self.drawingImages.append( image!)
+                
+            }
+        }
+    }
+    
 }
 
 
 struct Home: View {
     
     @ObservedObject private var vm = HomeViewModel()
+    @State var c:Int = 0
     
     var body: some View {
-
+        
         //Added scroll view for user's images
         VStack{
             HStack{
@@ -71,23 +125,31 @@ struct Home: View {
                 Image(systemName: "magnifyingglass")
             }.padding()
             
+            
             ScrollView{
-                VStack{
-                    HStack{
-                        HomeImage()
-                        HomeImage()
-                        HomeImage()
-                    }
-                    HStack{
-                        HomeImage()
-                        HomeImage()
-                        HomeImage()
-                    }
-                    
-                }
+                
+                // Citation :  ChatGPT
+                ForEach(vm.drawingImages.chunks(of: 3), id: \.self) { chunk in
+                                    HStack {
+                                        ForEach(chunk, id: \.self) { image in
+                                            Image(uiImage: image)
+                                                .resizable()
+                                                .frame(width: 100,height: 100)
+                                                .aspectRatio(contentMode: .fill)
+                                                .cornerRadius(30)
+                                                .padding()
+                                                .overlay(
+                                                    RoundedRectangle(cornerRadius: 16)
+                                                        .stroke(Color(red: 0.0, green: 0.6078431372549019, blue: 0.5098039215686274), lineWidth: 3))
+                                        }
+                                    }
+                                }
+                
             }
         }
     }
+
+    
 }
 
 struct Home_Previews: PreviewProvider {
