343aff8b167d772c0a1c6be4d6bf2bf5b9514e43
adeshagarwal28 <59112633+adeshagarwal28@users.noreply.github.com>
2023-04-03 00:18:54 -0400

    Created UserModel to fetch User Details (Currently inactive)

---
+++ Drawfully/Models/UserModel.swift
@@ -0,0 +1,19 @@
+//
+//  UserModel.swift
+//  Drawfully
+//
+//  Created by Adesh Agarwal on 4/2/23.
+// Citation : https://youtu.be/GMxo8MA6Nnc?list=PLdBY1aYxSpPVI3wTlK1cKHNOoq4JA3X5-
+
+import Foundation
+
+struct User: Encodable, Decodable{
+    var uid:String
+    var email: String
+    var profileImageUrl:String
+    var username:String
+    var searchName:[String]
+    var streak:String
+    
+    //var bio:String
+}
