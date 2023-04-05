f4152c4ebd173040ba43d3215d5ae20bdf82a1bc
adeshagarwal28 <59112633+adeshagarwal28@users.noreply.github.com>
2023-04-04 20:36:26 -0400

    Formatted Extensions

--- Drawfully/Utils/Extensions.swift
+++ Drawfully/Utils/Extensions.swift
@@ -7,11 +7,11 @@
 
 import Foundation
 
-extension Encodable{
+extension Encodable {
     func asDictionary() throws -> [String: Any] {
-        let data = try JSONEncoder ( ) . encode (self)
+        let data = try JSONEncoder().encode(self)
         
-        guard let dictionary = try JSONSerialization.jsonObject (with: data, options: .allowFragments) as? [String: Any]
+        guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
         else {
             throw NSError ()
         }
