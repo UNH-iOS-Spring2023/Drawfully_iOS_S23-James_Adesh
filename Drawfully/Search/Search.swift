044bfb4bfb50edf0417c9d61b995038e108d8242
hoodz <jmok780@gmail.com>
2023-04-08 00:58:18 -0400

    Search.swift + SearchQueries.swift: Updated to search items of a hard-coded list, functions for firebase queries

--- Drawfully/Search/Search.swift
+++ Drawfully/Search/Search.swift
@@ -4,71 +4,108 @@
 //
 //  Created by Adesh Agarwal on 2/17/23.
 //
+// Reference for User Search Field: https://www.hackingwithswift.com/quick-start/swiftui/how-to-add-a-search-bar-to-filter-your-data
 
 import SwiftUI
 
 struct Search: View {
+    @State private var search: String = ""
+    @State private var scope: String = "Users"
+    @State private var suggestionDisplay = ""
+    
+    private var users: [String] = firebaseUserQuery() // TODO make user references
+    private var favorites: [String] = firebaseUserSavedQuery() // TODO make drawing document references
+    private var scopeFields: [String] = ["Users", "Saved", "Suggestions"]
+    
     var body: some View {
-        
-        //Add title bar to Search Tab
-        
         // Nice looking header
         let header = VStack{
-                
                 HStack{
-                    
                     Spacer()
-                    Text("Search & Suggestions ").font(.title).fontWeight(.bold).padding(.trailing, 0.0).multilineTextAlignment(.center)
+                    Text("Inspiration ").font(.title).fontWeight(.bold).padding(.trailing, 0.0).multilineTextAlignment(.center)
                     Spacer()
-                
                 }.padding()
             }
         
-        // After searching, show related users or Titles
-        let dropdown = ScrollView{
-            Text("User").padding(3)
-            Text("User").padding(3)
-            Text("User").padding(3)
+        // create the list referenced by the search bar, with filters applied
+        var usersList: [String] {
+            if search.isEmpty{
+                return users
+            } else {
+                return users.filter{item in
+                    item.localizedCaseInsensitiveContains(search) } //if any part matches the current text in search, display it
+            }
         }
+       // After searching, show related Users or Drawings
+        let userSearch = NavigationStack{
+            List {
+                ForEach(usersList, id: \.self) { name in
+                    NavigationLink{
+                        Text(name) //TODO make visible user page
+                    } label: {
+                        Text(name)
+                    }
+                }
+            }.navigationTitle("Users")
+        }.searchable(text: $search).disableAutocorrection(true).textInputAutocapitalization(.never)
         
-        // Show all the posts user has saved
-        let posts = ScrollView {
-            HStack{
-                Text("Posts").padding()
-                Text("Posts").padding()
-                Text("Posts").padding()
-            }.padding()
-            HStack{
-                Text("Posts").padding()
-                Text("Posts").padding()
-                Text("Posts").padding()
-            }.padding()
-            HStack{
-                Text("Posts").padding()
-                Text("Posts").padding()
-                Text("Posts").padding()
-            }.padding()
-        }
+            
+            // create the array of all the saved drawings, adhering to search filters
+            var savedDrawingsList: [String] {
+                if search.isEmpty{
+                    return favorites
+                } else {
+                    return favorites.filter{item in
+                        item.localizedCaseInsensitiveContains(search) } //if any part matches the current text in search, display it
+                }
+            }
+        
+        // Display the drawings
+        let savedDrawings = NavigationStack{
+            List {
+                ForEach(savedDrawingsList, id: \.self) { name in
+                    NavigationLink{
+                        Text(name) //TODO make visible drawing page
+                    } label: {
+                        Text(name)
+                    }
+                }
+            }.navigationTitle("Saved")
+        }.searchable(text: $search).disableAutocorrection(true).textInputAutocapitalization(.never)
+    
         
         // Give random ideas from the database to the user
-        let suggestions = HStack{
-                Text("Get a Time Limit!").padding()
-                Text("Get a Theme!").padding()
-                Text("Get an Object!").padding()
+        let suggestions = VStack{
+            Button(action: getTime){
+                Text("Get a time limit!")
+            }.padding().buttonStyle(.bordered)
+            
+            Button(action: getTheme){
+                Text("Get a theme!")
+            }.padding().buttonStyle(.bordered)
+            
+            Button(action: getObject){
+                Text("Get a object!")
+            }.padding().buttonStyle(.bordered)
+            
+            Text(suggestionDisplay).padding().font(.system(size: 30)).multilineTextAlignment(.center)
             }
+        
+        
+    
         
         // Make the body of the UI
         let body = VStack {
-            Text("Search Bar").bold(true).underline()
-            dropdown
-            Divider()
-            
-            Text("Saved Posts").padding().bold(true).underline()
-            posts
-            Divider()
-            
-            Text("Suggestions")
-            suggestions
+            // Pick which item you want to use
+                Picker("Scope", selection: $scope) {
+                    ForEach(scopeFields, id:\.self){
+                        Text($0)
+                    }
+                }.pickerStyle(.segmented).padding()
+            if (scope == "Users"){ userSearch }
+            else if (scope == "Saved"){ savedDrawings }
+            else if (scope == "Suggestions"){ suggestions }
+            else { userSearch }
         }
         
         
@@ -81,6 +118,26 @@ struct Search: View {
             Spacer()
         }
         }
+    
+    // return a random predefined time
+    func getTime() {
+        let times = ["1 Minute", "5 Minutes", "10 Miuntes", "30 Minutes", "1 Hour", "1 Day"]
+        suggestionDisplay = "Time:\n" + times.randomElement()!
+    }
+    
+    // return a random predefined theme
+    // TODO connect this to database
+    func getTheme() {
+        let themes = ["Cartoon", "Realist", "Impressionist"]
+        suggestionDisplay = "Theme:\n" + themes.randomElement()!
+    }
+    
+    // return a random predefined object
+    // TODO connect this to the database
+    func getObject() {
+        let objects = ["Human", "Fruit", "Building", "What's in front of you", "Landscape", "Nature", "Animal"]
+        suggestionDisplay = "Object:\n" + objects.randomElement()!
+    }
     
     }
 
---
+++ Drawfully/Search/SearchQueries.swift
@@ -0,0 +1,24 @@
+//
+//  SearchUsers.swift
+//  Drawfully
+//
+//  Created by James on 4/7/23.
+//
+
+import Foundation
+
+//should be user models
+func firebaseUserQuery() -> [String]{
+    let query = ["hoodz", "cloode", "bear"]
+    
+    return query
+
+}
+
+
+//should be DrawingModels
+func firebaseUserSavedQuery() -> [String]{
+    let query = ["Falling Water", "Rainy Day", "Harmony"]
+    
+    return query
+}
