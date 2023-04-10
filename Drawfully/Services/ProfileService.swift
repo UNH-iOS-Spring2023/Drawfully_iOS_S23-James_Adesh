54a25816bd3dcecf8b381bd5c4cc68f34cfef5d1
adeshagarwal28 <59112633+adeshagarwal28@users.noreply.github.com>
2023-04-09 22:36:05 -0400

    Created ProfileService to fetch users' posts

---
+++ Drawfully/Services/ProfileService.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileService.swift
+//  Drawfully
+//
+//  Created by Adesh Agarwal on 4/6/23.
+//  Citation : The entire class is implemented with reference to the YouTube tutorial series - SwiftUI Instagram Clone with Firebase by iosMastery
+//  Citation : https://youtu.be/J1fd-b8vnpQ?list=PLdBY1aYxSpPVI3wTlK1cKHNOoq4JA3X5-
+
+import Foundation
+
+
+
+class ProfileService: ObservableObject{
+    
+    //Array of PostModel objects to store all user posts
+    @Published var posts: [PostModel]=[]
+    
+    //Function to fetch all posts made by the logged in user
+    func loadUserPosts(userId:String)
+    {
+        PostService.loadUserPosts(userId: userId){
+            posts in
+            
+            self.posts=posts
+        }
+    }
+}
