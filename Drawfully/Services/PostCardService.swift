079791d8a57150868f161e28388f9cb92bcce8b3
adeshagarwal28 <59112633+adeshagarwal28@users.noreply.github.com>
2023-04-14 04:13:49 -0400

    Correction in like and unlike functions

--- Drawfully/Services/PostCardService.swift
+++ Drawfully/Services/PostCardService.swift
@@ -33,11 +33,11 @@ class PostCardService: ObservableObject {
         post.likeCount+=1
         isLiked = true
         
-        PostService.PostsUserId(userId: post.ownerId).collection("posts").document(post.postId).updateData(["likeCount" : post.likeCount, "\(Auth.auth().currentUser!.uid)": true])
+        PostService.PostsUserId(userId: post.ownerId).collection("posts").document(post.postId).updateData(["likeCount" : post.likeCount, "likes.\(Auth.auth().currentUser!.uid)": true])
         
-        PostService.AllPosts.document(post.postId).updateData([ "likeCount" : post.likeCount, "\(Auth.auth().currentUser!.uid)": true])
+        PostService.AllPosts.document(post.postId).updateData([ "likeCount" : post.likeCount, "likes.\(Auth.auth().currentUser!.uid)": true])
         
-        PostService.TimelineUserId(userId: post.ownerId).collection("timeline").document(post.postId).updateData([ "likeCount" : post.likeCount, "\(Auth.auth().currentUser!.uid)": true])
+        PostService.TimelineUserId(userId: post.ownerId).collection("timeline").document(post.postId).updateData([ "likeCount" : post.likeCount, "likes.\(Auth.auth().currentUser!.uid)": true])
     }
     
     
@@ -47,11 +47,11 @@ class PostCardService: ObservableObject {
         post.likeCount-=1
         isLiked = false
         
-        PostService.PostsUserId(userId: post.ownerId).collection("posts").document(post.postId).updateData(["likeCount" : post.likeCount, "\(Auth.auth().currentUser!.uid)": false])
+        PostService.PostsUserId(userId: post.ownerId).collection("posts").document(post.postId).updateData(["likeCount" : post.likeCount, "likes.\(Auth.auth().currentUser!.uid)": false])
         
-        PostService.AllPosts.document(post.postId).updateData([ "likeCount" : post.likeCount, "\(Auth.auth().currentUser!.uid)": false])
+        PostService.AllPosts.document(post.postId).updateData([ "likeCount" : post.likeCount, "likes.\(Auth.auth().currentUser!.uid)": false])
         
-        PostService.TimelineUserId(userId: post.ownerId).collection("timeline").document(post.postId).updateData([ "likeCount" : post.likeCount, "\(Auth.auth().currentUser!.uid)": false])
+        PostService.TimelineUserId(userId: post.ownerId).collection("timeline").document(post.postId).updateData([ "likeCount" : post.likeCount, "likes.\(Auth.auth().currentUser!.uid)": false])
     }
     
 }
