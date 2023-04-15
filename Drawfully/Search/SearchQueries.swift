e74bab0c655f0d8a301e51fadd87f3d81c011af7
hoodz <jmok780@gmail.com>
2023-04-15 19:26:25 -0400

    SearchQueries.swift: updated user query to access firebase

--- Drawfully/Search/SearchQueries.swift
+++ Drawfully/Search/SearchQueries.swift
@@ -9,7 +9,22 @@ import Foundation
 
 //should be user models
 func firebaseUserQuery() -> [String]{
-    let query = ["hoodz", "cloode", "bear"]
+    var query: [String] = ["test"]
+        
+    FirebaseManager.shared.firestore.collection("users").getDocuments() { (querySnapshot, error) in
+        if let err = error {
+            print("Error Querying Users: \(err)")
+        } else {
+            for document in querySnapshot!.documents {
+                let value: String = document.get("username") as? String ?? "Unknown"
+                print(value)
+                print("\(document.documentID) => \(document.get("username"))")
+                query.append(value)
+            }
+        }
+    }
+    print(query)
+    query.sort()
     
     return query
 
