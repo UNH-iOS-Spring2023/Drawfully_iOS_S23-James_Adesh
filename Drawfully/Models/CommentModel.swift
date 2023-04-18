3097eb45413f4db108a4ff03541b7ad0028c0ffc
adeshagarwal28 <59112633+adeshagarwal28@users.noreply.github.com>
2023-04-17 22:01:05 -0400

    Made CommentModel

---
+++ Drawfully/Models/CommentModel.swift
@@ -0,0 +1,24 @@
+//
+//  CommentModel.swift
+//  Drawfully
+//
+//  Created by Adesh Agarwal on 4/14/23.
+//  Citation : The entire class is implemented with reference to the YouTube tutorial series - SwiftUI Instagram Clone with Firebase by iosMastery
+//  Citation :https://youtu.be/i9ZIPGpPENw?list=PLdBY1aYxSpPVI3wTlK1cKHNOoq4JA3X5-
+
+
+import Foundation
+
+struct CommentModel: Encodable, Decodable, Identifiable {
+    
+    //Creating a unique identifier
+    var id = UUID()
+    
+    var profileImageUrl: String
+    var postId: String
+    var username: String
+    var date:Double
+    var comment: String
+    var ownerId: String
+    
+}
