3d21d8b937929e6f9a7551e3d48fbf8cd60a0f30
hoodz <jmok780@gmail.com>
2023-04-18 19:29:06 -0400

    UserView.swift: added comments for clarity

--- Drawfully/Models/UserView.swift
+++ Drawfully/Models/UserView.swift
@@ -11,29 +11,30 @@ import FirebaseAuth
 import SDWebImageSwiftUI
 
 struct UserView: View {
-    var user: User
+    var user: User // information of the inputted user
     
-    @StateObject var profileService = ProfileService()
-    let threeColumns = [GridItem(), GridItem(), GridItem()]
+    @StateObject var profileService = ProfileService() // helps load user posts
+    let threeColumns = [GridItem(), GridItem(), GridItem()] // helps load posts onto screen
     
     var body: some View {
-        // NavigationView{
-        //Added scroll view for user's images
         VStack{
             HStack{
                 Image("streak").resizable().frame(width:30, height: 30)
-                
+
                 //Getting streak count from user's data
                 Text(String(user.streak))
                     .font(.title2)
                     .fontWeight(.bold)
                 Spacer()
+                // The title becomes the inputted username
                 Text(user.username).font(.title).fontWeight(.bold).padding(.trailing, 42.0).multilineTextAlignment(.center)
                 Spacer()
             }.padding()
+            // get the unique ID for the user
             Text(user.uid).font(.caption).fontWeight(.light).padding(.trailing).multilineTextAlignment(.center)
             Divider()
             
+            // display images, same as the implementation in Home.swift
             ScrollView{
                 //Displaying 3 photos in a row
                 LazyVGrid(columns: threeColumns) {
