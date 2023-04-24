2deba3cab03e74ecd51a7a3df4d69c609d8b70a6
adeshagarwal28 <59112633+adeshagarwal28@users.noreply.github.com>
2023-04-24 06:30:58 -0400

    Removed unnecessary comments on Home.swift

--- Drawfully/Home/Home.swift
+++ Drawfully/Home/Home.swift
@@ -12,62 +12,6 @@ import Firebase
 import FirebaseAuth
 import SDWebImageSwiftUI
 
-//View Model for making firebase calls and receiving snapshots
-// Citation : https://www.youtube.com/watch?v=yHngqpFpVZU&list=PL0dzCUj1L5JEN2aWYFCpqfTBeVHcGZjGw&index=7
-//class HomeViewModel: ObservableObject{
-//
-//    @Published var errorMessage:String=""
-//    @Published var streakCount:String="T"
-//    @Published var CurrentUser:User=User(uid: "", email: "", profileImageUrl: "", username: "", searchName: [], streak: 0, firstName: "", lastName: "")
-//    static let shared = HomeViewModel ()
-//
-//    init()
-//    {
-//        //fetchCurrentUser()
-//        //super.init()
-//    }
-//
-//    func fetchCurrentUser(){
-//
-//        // If the user hasn't posted the day before, reset their streak to 0
-//        // Preconditions: valid firebase user id
-//        // Postconditions: if nothing posted yesterday, firebase document has streak set to 0
-//        func CheckStreak(uid: String) {
-//            let storedDate = UserDefaults.standard.object(forKey: "lastDate") as? Date ?? Calendar.current.date(byAdding: .day, value: -2, to: Date.now)
-//
-//            // if the date is less than today && if the date is not yesterday
-//            if storedDate! < Calendar.current.startOfDay(for: Date.now) && !Calendar.current.isDateInToday(storedDate!.addingTimeInterval(86400)){
-//                FirebaseManager.shared.firestore.collection("users").document(uid).updateData(["streak" : 0])
-//                print("reset streak to 0")
-//                // TODO Screen notifying that streak is lost
-//                // TODO properly test the function, since the UserDefaults object can't be stored in simulator
-//            }
-//            else{
-//                print("streak is safe")
-//            }
-//        }
-//        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else{
-//            return}
-//
-//        CheckStreak(uid: uid)
-//
-//
-//        FirebaseManager.shared.firestore.collection("users").document(uid).getDocument{snapshot, error in
-//            if let error = error{
-//                print("Failed to fetch user data : \(error)")
-//            }
-//
-//            guard let data=snapshot?.data() else {
-//                return}
-//
-//            let appUser = User(uid: uid,  email: data["email"] as? String ?? "", profileImageUrl: data["profileImageUrl"] as? String ?? "", username: data["username"] as? String ?? "", searchName: data["searchName"] as? [String] ?? [""], streak: data["streak"] as? Int ?? 0, firstName: data["firstName"] as? String ?? "", lastName: data["lastName"] as? String ?? "")
-//            self.CurrentUser=appUser
-//
-//        }
-//    }
-//
-//}
-
 struct Home: View {
     
     @EnvironmentObject var session: SessionStore
@@ -76,11 +20,8 @@ struct Home: View {
     
     let threeColumns = [GridItem(), GridItem(), GridItem()]
     
-    //@ObservedObject private var vm = HomeViewModel()
-    
     var body: some View {
         
-       // NavigationView{
             //Added scroll view for user's images
             VStack{
                 HStack{
