7842e8761f209260d7752834a260d62cd45f7c91
hoodz <jmok780@gmail.com>
2023-05-08 14:28:50 -0400

    Added Clock view and Card class

---
+++ Drawfully/Search/Clock.swift
@@ -0,0 +1,20 @@
+//
+//  Timer.swift
+//  Drawfully
+//
+//  Created by James on 5/8/23.
+//
+
+import SwiftUI
+
+struct Timer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Timer_Previews: PreviewProvider {
+    static var previews: some View {
+        Timer()
+    }
+}
